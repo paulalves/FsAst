b88b1be8a9bb041353846c2d6fe93f5e59cd4334
Paul Alves <paulalves@protonmail.com>
2023-09-17 17:29:52 +0800

    parsing finally works

--- src/FsAst.Library/Library.fs
+++ src/FsAst.Library/Library.fs
@@ -25,52 +25,72 @@ module internal Parser =
     let private regex = Regex @"((?<token>(\d+|\w+|\^|\+|-|\*|/)))"
 
     let private isDigit (ch: string) = ch.[0] |> System.Char.IsDigit
-
-    let tokenize (input: string) =
-        [ for x in regex.Matches(input) do
-              let token =
-                  match x.Groups.["token"].Value with
-                  | "+" -> Add
-                  | "-" -> Subtract
-                  | "*" -> Multiply
-                  | "^" -> Power
-                  | "/" -> Division
-                  | ch when isDigit ch -> Number(int ch)
-                  | ch -> Identifier(ch)
-              yield token ]
+   
+    let tokenize (input: string) : Token list =
+        // todo: remember how to write active patterns and remove this little ugly thing.
+        let tokenMap = 
+            dict [
+                ("+", Add); 
+                ("-", Subtract); 
+                ("*", Multiply); 
+                ("^", Power); 
+                ("/", Division)
+            ]
+        
+        let peekNextToken currentIndex =
+            let m = regex.Match(input, currentIndex)
+            if not m.Success then None
+            else
+                match m.Groups.["token"].Value with
+                | ch when tokenMap.ContainsKey ch -> Some (tokenMap.[ch])
+                | ch when isDigit ch -> Some (Number(int ch))
+                | ch -> Some (Identifier(ch))
+    
+        let tokens =
+            [ for x in regex.Matches(input) |> Seq.cast<Match> do
+                match x.Groups.["token"].Value with
+                | ch when tokenMap.ContainsKey ch -> yield tokenMap.[ch]
+                | ch when isDigit ch -> 
+                    yield Number(int ch)
+                    if peekNextToken (x.Index + x.Length)
+                       |> Option.exists (function Identifier _ -> true | _ -> false) then
+                        yield Multiply
+                | ch -> yield Identifier(ch)
+            ]
+        tokens
 
     let private read (stream: TokenStream) =
         match stream with
         | head :: tail -> Some(head, tail)
         | _ -> None
 
-    let rec private parseTerm (stream: TokenStream) =
+    let rec private parseFactor (stream: TokenStream) =
         match read stream with
         | Some(Number number, stream) -> Const(number), stream
         | Some(Identifier id, stream) -> Var(id), stream
         | _ -> failwith "Unexpected token"
 
-    let rec parsePower (stream: TokenStream) =
-        let lhs, stream' = parseTerm stream
+    let rec private parsePower (stream: TokenStream) =
+        let lhs, stream' = parseFactor stream
         match read stream' with
         | Some(Power, stream'') ->
             let rhs, stream''' = parsePower stream''
             Op(lhs, "^", rhs), stream'''
         | _ -> lhs, stream'
 
-    let rec parseFactor (stream: TokenStream) =
+    let rec private parseProduct (stream: TokenStream) =
         let lhs, stream' = parsePower stream
         match read stream' with
         | Some(Multiply, stream'') ->
-            let rhs, stream''' = parseFactor stream''
+            let rhs, stream''' = parseProduct stream''
             Op(lhs, "*", rhs), stream'''
         | Some(Division, stream'') ->
-            let rhs, stream''' = parseFactor stream''
+            let rhs, stream''' = parseProduct stream''
             Op(lhs, "/", rhs), stream'''
         | _ -> lhs, stream'
 
     let rec parseExpr (stream: TokenStream) =
-        let lhs, stream' = parseFactor stream
+        let lhs, stream' = parseProduct stream
         match read stream' with
         | Some(Add, stream'') ->
             let rhs, stream''' = parseExpr stream''
--- tests/FsAst.Tests/Tests.fs
+++ tests/FsAst.Tests/Tests.fs
@@ -7,35 +7,45 @@ module Tests =
     open FsAst.Library
     open FsAst.Library.Exp
 
-    let validTokenizationSamples : obj[] seq =
-        [ yield [| "x + y ^ x * y"; [| Identifier "x"; Add; Identifier "y"; Power; Identifier "x"; Multiply; Identifier "y" |] |]
+    let validTokenizationSamples: obj[] seq =
+        [ 
+          yield [| "5"; [| Number 5 |] |]          
+          yield [| "3x"; [| Number 3; Multiply; Identifier "x" |] |]
           yield [| "x ^ 2 + 1"; [| Identifier "x"; Power; Number 2; Add; Number 1 |] |]
-          yield [| "3x"; [| Number 3; Identifier "x" |] |]
-          yield [| "5"; [| Number 5 |] |]
-          yield [| "x ^ 2 + 3x - 5"; [| Identifier "x"; Power; Number 2; Add; Number 3; Identifier "x"; Subtract; Number 5 |] |]
+          yield [| "x + y ^ x * y"; [| Identifier "x"; Add; Identifier "y"; Power; Identifier "x"; Multiply; Identifier "y" |] |]          
+          yield [| "x ^ 2 + 3x - 5"; [| Identifier "x"; Power; Number 2; Add; Number 3; Multiply; Identifier "x"; Subtract; Number 5 |] |]
           yield [| "x ^ 3 ^ 2"; [| Identifier "x"; Power; Number 3; Power; Number 2 |] |]
         ]
 
-    let validParsingSamples : obj[] seq =
-        [ yield [| "x"; Var "x" |]
+    let validParsingSamples: obj[] seq =
+        [
+          yield [| "x"; Var "x" |]
+          yield [| "3x"; Op(Const 3, "*", Var "x") |]
           yield [| "42"; Const 42 |]
           yield [| "x ^ 3"; Op(Var "x", "^", Const 3) |]
           yield [| "x + y"; Op(Var "x", "+", Var "y") |]
           yield [| "x - y"; Op(Var "x", "-", Var "y") |]
           yield [| "x * y"; Op(Var "x", "*", Var "y") |]
           yield [| "x / y"; Op(Var "x", "/", Var "y") |]
+          yield [| "3x + y"; Op(Op(Const 3, "*", Var "x"), "+", Var "y") |]
+          yield [| "x ^ 2 + y"; Op(Op(Var "x", "^", Const 2), "+", Var "y") |]
+          yield [| "2 + 1 * 3"; Op(Const 2, "+", Op(Const 1, "*", Const 3)) |]
           yield [| "x ^ 3 ^ 2"; Op(Var "x", "^", Op(Const 3, "^", Const 2)) |]          
-          //yield [| "x ^ 2 + 3x - 5"; Op(Op(Op(Var "x", "^", Const 2), "+", Op(Const 3, "*", Var "x")), "-", Const 5) |]
+          yield [| "2x * 3y + z"; Op(Op(Const 2, "*", Op(Var "x", "*", Op(Const 3, "*", Var "y"))), "+", Var "z") |]
         ]
-        
-    [<Theory>]
-    [<ExcludeFromCodeCoverage>]
-    [<MemberData(nameof(validTokenizationSamples))>]
-    let ``when received valid input it should tokenize it`` input expected =
-        input |> tokenize |> should equalSeq expected
 
     [<Theory>]
     [<ExcludeFromCodeCoverage>]
-    [<MemberData(nameof(validParsingSamples))>]
+    [<MemberData(nameof (validTokenizationSamples))>]
+    let ``when received valid input it should tokenize it`` input expected =
+        input
+        |> tokenize
+        |> should equalSeq expected
+
+    [<Theory>]
+    [<ExcludeFromCodeCoverage>]
+    [<MemberData(nameof (validParsingSamples))>]
     let ``when received valid input it should parse it`` input expected =
-        input |> parse |> should equal expected
+        input
+        |> parse
+        |> should equal expected
