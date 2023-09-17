05b1bdadf5e1e30a48a099609bb5f11159f44ff9
Paul Alves <paulalves@protonmail.com>
2023-09-17 19:21:35 +0800

    csharp friendly export types

---
+++ src/FsAst.Types/Types.fs
@@ -0,0 +1,25 @@
+﻿namespace FsAst.Types
+
+open FsAst.Library
+
+(* Non Idiomatic F# for OOP fanatics and C# ppl who can't Function *)
+
+type Lexer =
+    member _.Lex(input) : TokenStream = input |> tokenize
+
+type Parser(lexer: Lexer) =
+    member this.Log exp : string = exp |> this.Parse |> sprintf "%A"
+
+    member _.Parse exp : Term = exp |> lexer.Lex |> parseTokens
+
+type Interpreter (context, parser : Parser) =    
+    member this.Log exp =
+        let ast = parser.Parse exp |> sprintf "%A"
+        let res = this.Interpret exp |> sprintf "%A"
+        sprintf "(Ast = [ %s ], Result = [ %s ])" ast res
+
+    member _.Interpret exp : decimal =
+        evalAst context (
+            exp
+            |> parser.Parse            
+        ) |> decimal
